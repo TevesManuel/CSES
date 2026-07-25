988cc6fc0867ebe614f5176ff3cb2b37b24d3c20
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2026-07-25 14:01:49 -0300

    upgrade my template

--- 0_MyTemplate/src/app.cpp
+++ 0_MyTemplate/src/app.cpp
@@ -2,16 +2,18 @@
 
 #include <bits/stdc++.h>
 
-#define dbg(x) std::cout << #x << " = " << (x) << ", "
-#define dbgn(x) std::cout << #x << " = " << (x) << std::endl
-#define lineN(n) for(int teves = 0; teves < n; teves++) std::cout << "="; std::cout << std::endl
-#define line lineN(60)
-
 typedef long long ll;
 typedef long double ld;
-
 const int inf = 1 << 30;
 // const ll inf = 1LL << 60;
+
+#define forAB(i, a, b) for(ll i = (a); i < (ll)(b); i++)
+#define forN(i, n) forAB(i, 0, (n))
+
+#define dbg(x) std::cout << #x << " = " << (x) << ", "
+#define dbgn(x) std::cout << #x << " = " << (x) << std::endl
+#define lineN(n) forN(teves, (n)) std::cout << "="; std::cout << std::endl
+#define line lineN(60)
 
 #define TEVES std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
 
