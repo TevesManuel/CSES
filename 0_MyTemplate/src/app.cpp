b260c6cf86b28426bb7ac9c4100939b0083c0d27
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2026-07-25 13:55:12 -0300

    create my own template

---
+++ 0_MyTemplate/src/app.cpp
@@ -0,0 +1,31 @@
+/* AUTHOR: Manuel Tomas Teves */
+
+#include <bits/stdc++.h>
+
+#define dbg(x) std::cout << #x << " = " << (x) << ", "
+#define dbgn(x) std::cout << #x << " = " << (x) << std::endl
+#define lineN(n) for(int teves = 0; teves < n; teves++) std::cout << "="; std::cout << std::endl
+#define line lineN(60)
+
+typedef long long ll;
+typedef long double ld;
+
+const int inf = 1 << 30;
+// const ll inf = 1LL << 60;
+
+#define TEVES std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
+
+int main() {
+    TEVES
+
+    int n, k;
+    std::cin >> n >> k;
+
+    dbg(n);
+    dbgn(k);
+    line;
+    dbg(n);
+    dbgn(k);
+
+    return 0;
+}
