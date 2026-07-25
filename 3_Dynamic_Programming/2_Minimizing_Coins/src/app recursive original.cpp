44da16208b31debb05017985283609651d7fc708
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2026-07-25 16:51:52 -0300

    Minimizing Coins solved

---
+++ 3_Dynamic_Programming/2_Minimizing_Coins/src/app recursive original.cpp
@@ -0,0 +1,101 @@
+/* AUTHOR: Manuel Tomas Teves */
+
+//Time Limit Exceded
+//O(m*k) aprox 10e8
+//Programacion recursiva consume demasiados recursos, se mejoro a iterativo
+
+#include <bits/stdc++.h>
+
+//Types
+typedef long long ll;
+typedef long double ld;
+typedef int8_t   i8;          // -128 a 127
+typedef uint8_t  u8;          // 0 a 255
+typedef int16_t  i16;         // -32.768 a 32.767
+typedef uint16_t u16;         // 0 a 65.535
+typedef int32_t  i32;         // -2.147.483.648 a 2.147.483.647
+typedef uint32_t u32;         // 0 a 4.294.967.295
+typedef int64_t  i64;         // -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+typedef uint64_t u64;         // 0 a 18.446.744.073.709.551.615
+typedef __int128_t  i128;     // -(2^127) a 2^127 - 1
+typedef __uint128_t u128;     // 0 a 2^128 - 1
+typedef float       f32;      // ±3.402823466 × 10^38
+typedef double      f64;      // ±1.797693134 × 10^308
+typedef __float128  f128;     // ±1.189731495 × 10^4932
+
+//Constants
+const int inf = 1 << 30;
+// const ll inf = 1LL << 60;
+
+//Iterative utils
+#define forAB(i, a, b) for(ll i = (a); i < (ll)(b); i++)
+#define forN(i, n) forAB(i, 0, (n))
+
+//Vector utils
+#define sz(vec) int((vec).size())
+#define pb push_back
+#define all(vec) (vec).begin(), (vec).end()
+
+//Debug utils
+#define dbg(x) std::cout << #x << " = " << (x) << ", "
+#define dbgn(x) std::cout << #x << " = " << (x) << std::endl
+#define lineN(n) forN(teves, (n)) std::cout << "="; std::cout << std::endl
+#define line lineN(60)
+
+#define TEVES std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
+
+std::vector<i32> memory;
+
+i32 r(std::vector<i32> &coins, int k) {
+    if(k == 0) {
+        return 0;
+    }
+
+    if(memory[k] != -1)
+        return memory[k];
+
+    // dbg(memory[k]);
+    // dbgn(k);
+
+    int best = inf;
+    int rsp;// response
+    int size = sz(coins);
+    forN(i, size) {
+        if(coins[i] > k)
+            break; // pq esta ordenado el vector de monedas
+
+        rsp = r(coins, k - coins[i]);
+        if(rsp != -1) {
+            best = std::min(best, rsp + 1);
+        }
+    }
+
+    if(best == inf)
+        return -1;
+
+
+    return memory[k] = best;
+}
+
+i32 main() {
+    TEVES
+
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<i32> coins(n);
+    memory.resize(k + 1);
+    std::fill(all(memory), -1);
+
+    forN(i, n) {
+        std::cin >> coins[i];
+    }
+
+    std::sort(all(coins));
+    
+    int q = r(coins, k);
+
+    std::cout << q << "\n";
+
+    return 0;
+}
