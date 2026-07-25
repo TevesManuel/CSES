a1b1545766a0bfae70f6763e73460b9b0838800c
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2026-07-25 14:19:53 -0300

    upgrade my template

--- 0_MyTemplate/src/app.cpp
+++ 0_MyTemplate/src/app.cpp
@@ -2,14 +2,24 @@
 
 #include <bits/stdc++.h>
 
+//Types
 typedef long long ll;
 typedef long double ld;
+
+//Constants
 const int inf = 1 << 30;
 // const ll inf = 1LL << 60;
 
+//Iterative utils
 #define forAB(i, a, b) for(ll i = (a); i < (ll)(b); i++)
 #define forN(i, n) forAB(i, 0, (n))
 
+//Vector utils
+#define sz(vec) int((vec).size())
+#define pb push_back
+#define all(vec) (vec).begin(), (vec).end()
+
+//Debug utils
 #define dbg(x) std::cout << #x << " = " << (x) << ", "
 #define dbgn(x) std::cout << #x << " = " << (x) << std::endl
 #define lineN(n) forN(teves, (n)) std::cout << "="; std::cout << std::endl
